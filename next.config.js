47734dcdb837cea7d5c0353c8235627be076f3fe
in-mansi-gupta <in.mansi.gupta@gmail.com>
2026-04-05 18:43:06 +0530

    Fixing deployment issue.

--- next.config.js
+++ next.config.js
@@ -2,12 +2,9 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  // Remove basePath if deploying to username.github.io
-  // Add basePath: '/repo-name' if deploying to username.github.io/repo-name
-  // basePath: '/mansi-portfolio',
+  images: { unoptimized: true },
+  basePath: '/MansiGuptaPortfolio',
+  assetPrefix: '/MansiGuptaPortfolio/',
 }
 
 module.exports = nextConfig
